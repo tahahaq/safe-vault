c2c276440c9b1c16b022b02a52c23c032a5ff312
taha <haqtaha@gmail.com>
2019-05-22 11:10:18 +0500

    added file share

--- src/azure/upload.js
+++ src/azure/upload.js
@@ -4,32 +4,46 @@ var fs = require('fs');
 
 var fileService = azure.createFileService('safevault', 'FVRq9jdWI4XXbE6EZ/FAypP5M6KxcVpwTwKT1LrNwS7Ei42NicH4UWDuNkF8bd9MLDMvWNVlr5pv+LufuJN+Ug==');
 
-test();
+
+async function uploadFile(filePath , fileName ,fileHash , ethereumAddress) {
+    try{
+        await  fileService.createShareIfNotExists('taskshare', function(error, result, response) {
+            if (!error) {
+                // if result = true, share was created.
+                // if result = false, share already existed.
+            }
+        });
+
+        await fileService.createDirectoryIfNotExists('taskshare', ethereumAddress, function(error, result, response) {
+            if (!error) {
+                // if result.created = true, share was created.
+                // if result.created = false, share already existed.
+            }
+        });
+
+
+        await fileService.createFileFromLocalFile('taskshare', 'taskdirectory', 'image', 'test.png', function(error, result, response) {
+            if (!error) {
+                // file uploaded
+            }
+        });
+
+
+    }catch (e) {
+
+    }
+};
+
+
 async function test() {
 
     fileService.listFilesAndDirectoriesSegmented('taskshare','taskdirectory',null , function(error,result,reponse) {
         console.log(result.entries.files)
     })
-    // await  fileService.createShareIfNotExists('taskshare', function(error, result, response) {
-    //     if (!error) {
-    //         // if result = true, share was created.
-    //         // if result = false, share already existed.
-    //     }
-    // });
-    //
-    // await fileService.createDirectoryIfNotExists('taskshare', 'taskdirectory', function(error, result, response) {
-    //     if (!error) {
-    //         // if result.created = true, share was created.
-    //         // if result.created = false, share already existed.
-    //     }
-    // });
-    //
-    //
-    // await fileService.createFileFromLocalFile('taskshare', 'taskdirectory', 'image', 'test.png', function(error, result, response) {
-    //     if (!error) {
-    //         // file uploaded
-    //     }
-    // });
+
+
+
+
 
 
 }
